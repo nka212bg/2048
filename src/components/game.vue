b1592a1c1bf8cf4b4840037c034632e7a558b4ad
nka212 <nka212bg@gmail.com>
2019-11-23 03:02:21 -0500

    _

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 500px; margin: auto">
     <!-- header -->
-    <div style="color: gray">
+    <div style="color: gray; width: 512px;">
       <div class="space-between" style="margin:70px 0">
         <div>
           <h1 style="font-size: 2.5em;">2048</h1>
@@ -18,23 +18,32 @@
           </div>
         </div>
       </div>
-      <p class="button bold" style="margin-bottom: 30px"><i class="icon">add_circle</i> New Game</p>
+      <div>
+        <p class="button bold" style="margin-bottom: 40px" @click="init"><i class="icon">add_circle</i> New Game</p>
+
+        <select v-model="cellRow" style="margin-left: 20px;outline: none;border: none;background: none;">
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        cells
+      </div>
     </div>
 
-    <div id="game" class="blue-container flex-center" style="width: 500px; height: 500px; flex-wrap: wrap;">
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span> 
-      <span>0</span>
-      <span>0</span> 
+    <div id="game" class="flex-center" style="width: 500px; height: 500px; flex-wrap: wrap; border: 6px solid lightgray; border-radius: 10px">
       <span>0</span>
       <span>0</span>
       <span>0</span>
       <span>0</span>
       <span>0</span>
-      <span>0</span> 
+      <span>0</span>
+      <span>0</span>
+      <span>0</span>
+      <span>0</span>
+      <span class="active" style="background: green">0</span>
+      <span>0</span>
+      <span>0</span>
+      <span>0</span>
       <span>0</span>
       <span>0</span>
       <span>0</span>
@@ -42,17 +51,60 @@
   </div>
 </template>
 
-<script> 
+<script>
 export default {
   data() {
     return {
+      cellRow: 4,
       score: 0,
-      best: 0
+      best: 0,
+      cellVal: []
     };
   },
   methods: {
     init() {
-      console.log("eeeeeeeeee");
+      this.cellVal = [];
+      this.game();
+    },
+    game() {
+      var newNum = this.randN();
+      var gameContainer = document.getElementById("game");
+      var cells = this.cellRow * this.cellRow;
+      var cellSize = 100 / this.cellRow;
+      gameContainer.innerHTML = "";
+      var evt = false;
+
+    if(!evt){
+      ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
+              document.body.addEventListener("keyup", () => { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                if (event.key === e) {
+                  evt = true;
+                  for (let i = 0; i < cells; i++) {
+                    var cellId = `${Math.ceil((i + 1) / this.cellRow)}.${(i + 1) % this.cellRow || this.cellRow}`;
+                    i + 1 === newNum ? (this.cellVal[i] = 2) : (this.cellVal[i] = 0);
+                    gameContainer.appendChild(this.cell(cellSize, cellId, this.cellVal[i]));
+                  }
+                  console.log(this.cellVal, newNum, e);
+                }
+              },{once: true});
+            });
+
+
+
+
+    }
+      
+    },
+    cell(cellSize, cellId, cellVal) {
+      var cell = document.createElement("SPAN");
+      cell.id = cellId;
+      cell.style = `width: ${cellSize}%; height: ${cellSize}%`;
+      cellVal !== 0 ? ((cell.style.background = `hsl(${(360 / 100) * cellVal}, 100%, 30%)`), (cell.style.color = "white")) : ((cell.style.background = "none"), (cell.style.color = "none"));
+      cell.innerHTML = cellVal;
+      return cell;
+    },
+    randN() {
+      return Math.floor(Math.random() * (this.cellRow * this.cellRow)) + 1;
     }
   },
   mounted() {
