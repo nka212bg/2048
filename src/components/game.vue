bab9b113cd6a443a7c507d5ee65c4fc3a8e3423d
nka212 <nka212bg@gmail.com>
2019-11-23 15:36:24 -0500

    _

--- src/components/game.vue
+++ src/components/game.vue
@@ -18,35 +18,29 @@
           </div>
         </div>
       </div>
-      <div>
-        <p class="button bold" style="margin-bottom: 40px" @click="init"><i class="icon">add_circle</i> New Game</p>
-
-        <select v-model="cellRow" style="margin-left: 20px;outline: none;border: none;background: none;">
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-        cells
+      <div class="space-between">
+        <div>
+          <p class="button bold" style="margin-bottom: 40px" @click="newGame"><i class="icon">add_circle</i> New Game</p>
+          <select v-model="cellRow" style="margin-left: 20px;outline: none;border: none;background: none;">
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          cells
+        </div>
+        <div style="font-size: 1.3em; margin-top: -40px; color: #c2c2c2">
+          <p class="icon" :style="`text-align: center; width: 100%; margin-bottom: -5px; ${keyDirection === 'ArrowUp' ? 'color: black' : ''}`">arrow_drop_up</p>
+          <div>
+            <span class="icon" :style="`${keyDirection === 'ArrowLeft' ? 'color: black' : ''}`">arrow_left</span>
+            <span class="icon" :style="`${keyDirection === 'ArrowDown' ? 'color: black' : ''}`">arrow_drop_down</span>
+            <span class="icon" :style="`${keyDirection === 'ArrowRight' ? 'color: black' : ''}`">arrow_right</span>
+          </div>
+        </div>
       </div>
     </div>
-
+    <!-- game container -->
     <div id="game" class="flex-center" style="width: 500px; height: 500px; flex-wrap: wrap; border: 6px solid lightgray; border-radius: 10px">
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span class="active" style="background: green">0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
-      <span>0</span>
+      <span style="width: 100%; height: 100%; cursor: pointer" @click="newGame">START NEW GAME</span>
     </div>
   </div>
 </template>
@@ -58,57 +52,73 @@ export default {
       cellRow: 4,
       score: 0,
       best: 0,
-      cellVal: []
+      cellVal: [],
+      cells: null,
+      cellSize: null,
+      gameContainer: null,
+      gameStarted: false,
+      keyDirection: "ArrowUp"
     };
   },
   methods: {
-    init() {
+    newGame() {
       this.cellVal = [];
+      this.cells = this.cellRow * this.cellRow;
+      this.cellSize = 100 / this.cellRow;
+      this.gameContainer = document.getElementById("game");
+
       this.game();
+      this.rewriteGame();
     },
+
     game() {
-      var newNum = this.randN();
-      var gameContainer = document.getElementById("game");
-      var cells = this.cellRow * this.cellRow;
-      var cellSize = 100 / this.cellRow;
-      gameContainer.innerHTML = "";
-      var evt = false;
-
-    if(!evt){
-      ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
-              document.body.addEventListener("keyup", () => { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                if (event.key === e) {
-                  evt = true;
-                  for (let i = 0; i < cells; i++) {
-                    var cellId = `${Math.ceil((i + 1) / this.cellRow)}.${(i + 1) % this.cellRow || this.cellRow}`;
-                    i + 1 === newNum ? (this.cellVal[i] = 2) : (this.cellVal[i] = 0);
-                    gameContainer.appendChild(this.cell(cellSize, cellId, this.cellVal[i]));
-                  }
-                  console.log(this.cellVal, newNum, e);
-                }
-              },{once: true});
-            });
-
-
-
-
-    }
-      
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        document.body.addEventListener("keyup", () => {
+          ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
+            if (event.key === e) (this.keyDirection = e), this.rewriteGame();
+          });
+        });
+      }
     },
-    cell(cellSize, cellId, cellVal) {
+
+    rewriteGame() {
+      this.gameContainer.innerHTML = "";
+      var newNum = this.randN();
+      for (let i = 0; i < this.cells; i++) {
+        var cellId = `${Math.ceil((i + 1) / this.cellRow)}.${(i + 1) % this.cellRow || this.cellRow}`;
+        i + 1 === newNum ? (this.cellVal[i] = 2) : (this.cellVal[i] = 0);
+        this.gameContainer.appendChild(this.cell(cellId, this.cellVal[i]));
+      }
+      console.log(this.cellVal, newNum, this.keyDirection);
+    },
+
+    cell(cellId, cellVal) {
       var cell = document.createElement("SPAN");
       cell.id = cellId;
-      cell.style = `width: ${cellSize}%; height: ${cellSize}%`;
+      cell.style = `width: ${this.cellSize}%; height: ${this.cellSize}%`;
       cellVal !== 0 ? ((cell.style.background = `hsl(${(360 / 100) * cellVal}, 100%, 30%)`), (cell.style.color = "white")) : ((cell.style.background = "none"), (cell.style.color = "none"));
       cell.innerHTML = cellVal;
       return cell;
     },
+
     randN() {
       return Math.floor(Math.random() * (this.cellRow * this.cellRow)) + 1;
     }
+
+    // game() {
+    //   var gameRefresh = () => {
+    //     ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
+    //       if (event.key === e) this.rewriteGame(e);
+    //     });
+    //   };
+
+    //   if (!this.gameStarted) {
+    //     this.gameStarted = true;
+    //     document.body.addEventListener("keyup", gameRefresh);
+    //   }
+    // },
   },
-  mounted() {
-    this.init();
-  }
+  mounted() {}
 };
 </script>
