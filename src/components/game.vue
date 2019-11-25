8ba436cca2505a1879c9b14533f09b35b8b69a6f
nka212 <nka212bg@gmail.com>
2019-11-25 14:19:20 -0500

    beta version

--- src/components/game.vue
+++ src/components/game.vue
@@ -23,7 +23,7 @@
       </div>
       <div class="space-between">
         <div>
-          <p class="button bold" style="margin-bottom: 40px" @click="newGame"><i class="icon">games</i> New Game</p>
+          <p class="button bold" style="margin-bottom: 40px" @click="newGame"><i class="icon">sports_esports</i> New Game</p>
           <select v-model="cellRow" style="margin-left: 20px;outline: none;border: none;background: none;">
             <option value="3">3</option>
             <option value="4">4</option>
@@ -32,6 +32,7 @@
           </select>
           cells
         </div>
+
         <div style="font-size: 1.3em; margin-top: -53px; color: #c2c2c2">
           <p class="icon" :style="`text-align: center; width: 100%; margin-bottom: -13px; ${keyDirection === 'ArrowUp' ? 'color: black' : ''}`">arrow_drop_up</p>
           <div>
@@ -44,13 +45,15 @@
     </div>
     <!-- game container -->
     <div id="game" class="flex-center" style="width: 500px; height: 500px; flex-wrap: wrap; border: 6px solid lightgray; border-radius: 10px">
-      <span style="width: 100%; height: 100%; cursor: pointer" @click="newGame">START NEW GAME</span>
+      <span style="width: 100%; height: 100%; cursor: pointer" @click="newGame"><i class="icon" style="padding-right: 20px; font-size: 3em; color: dodgerblue">sports_esports</i>START NEW GAME</span>
     </div>
     <!-- footer -->
-    <div style="margin-top: 20px ;font-size: 0.8em; color: gray">
-      <p>* Easy - all the adding operations during the game.</p>
-      <p>* Hard - the sum of all the values at the field.</p>
-    </div>
+    <transition>
+      <div style="margin: 20px ;font-size: 0.8em; color: gray">
+        <p>* Easy - the sum of all the adding operations during the game.</p>
+        <p>* Hard - the sum of all the values at the field.</p>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -72,6 +75,7 @@ export default {
     };
   },
   methods: {
+    /** New game */
     newGame() {
       this.scoreEasy = 0;
       this.scoreHard = 0;
@@ -80,42 +84,50 @@ export default {
       this.cellSize = 100 / this.cellRow;
       this.gameContainer = document.getElementById("game");
 
-      this.game();
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        document.body.addEventListener("keydown", this.game);
+      }
       this.rewriteGame();
     },
 
+    /** Inint method */
     game() {
-      if (!this.gameStarted) {
-        this.gameStarted = true;
-        document.body.addEventListener("keyup", () => {
-          ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
-            if (event.key === e) (this.keyDirection = e), this.rewriteGame();
-          });
-        });
-      }
+      ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].forEach(e => {
+        if (event.key === e) {
+          event.preventDefault();
+          this.keyDirection = e;
+          this.rewriteGame();
+        }
+      });
     },
 
+    /** Game engine
+     *
+     */
     rewriteGame() {
+      // clear the game field
       this.gameContainer.innerHTML = "";
+
+      // creates a temporary array and pre populate nested arrays at the count of cellRow value
       var tempArr = [];
       for (let i = 0; i < this.cellRow; i++) {
         tempArr.push([]);
       }
 
-      // this.cellVal[] to array with nested arrays
+      // populates the temp array with the values from this.cellVal[] and orienting it vertically || horizontally according to the key pressed
       for (let i = 0; i < this.cells; i++) {
         var row = Math.ceil((i + 1) / this.cellRow) - 1;
         var col = i % this.cellRow;
         this.keyDirection === "ArrowRight" || this.keyDirection === "ArrowLeft" ? (tempArr[row][col] = this.cellVal[i] || 0) : (tempArr[col][row] = this.cellVal[i] || 0);
       }
 
-      // calculate nested arrays separately ltr (push all the vals to right and merge equal vals)
+      // calculate nested arrays in the temp array separately [ltr] or [rtl] (push all the vals to the end and merge equal vals)
       tempArr.forEach(e => {
         for (let w = 0; w < e.length; w++) {
           if (this.keyDirection === "ArrowRight" || this.keyDirection === "ArrowDown") {
             // move all zeros to the beginning [ltr]
             e = orderZerosInArray(e);
-
             // add the equal vals that sits to eachother [ltr]
             if (e[w] === e[w + 1] && e[w] !== 0) {
               var cellResult = e[w + 1] * 2;
@@ -147,9 +159,9 @@ export default {
         }
       });
 
+      // local helper for this function (shifts the zeros to the end)
       function orderZerosInArray(arr, rtl = false) {
         if (Number(arr.join("")) === 0) return arr;
-
         if (rtl) {
           for (let i = arr.length; i >= 0; i--) {
             if (arr[i] === 0) {
@@ -157,15 +169,6 @@ export default {
               arr.splice(arr.length, 0, 0);
             }
           }
-          /** alternative way */
-          // arr.reverse();
-          // for (let i = 0; i < arr.length; i++) {
-          //   if (arr[i] === 0) {
-          //     arr.splice(i, 1);
-          //     arr.splice(0, 0, 0);
-          //   }
-          // }
-          // arr.reverse();
         } else {
           for (let i = 0; i < arr.length; i++) {
             if (arr[i] === 0) {
@@ -194,16 +197,23 @@ export default {
         }
       }
 
+      // New number to play with
       var newNum = this.nextNumber();
+
+      // GAME OVER
       if (!newNum) {
-        // GAME OVER
+        this.gameStarted = false;
+        document.body.removeEventListener("keydown", this.game);
         return (document.getElementById("game").innerHTML = `<span style="width: 100%; height: 100%; text-align: center;">GAME OVER<br><span style="font-size: 0.5em; width: 50%; box-shadow: none; font-weight: normal; padding-right: 20px;">Your score is ${this.level === "easy" ? this.scoreEasy : this.scoreHard} points</span></span>`);
       }
+
+      // populates the game field with the values of this.cellVal[]
       for (let i = 0; i < this.cells; i++) {
         var newCell = null;
         if (i + 1 === newNum) {
           this.cellVal[i] = 2;
           newCell = this.cell(2);
+          newCell.className = "enter";
           newCell.style.boxShadow = "lightgrey 0px 0px 0px 8px inset, green 0px 0px 0px 10px inset";
           newCell.style.background = "white";
           newCell.style.color = "green";
@@ -216,6 +226,7 @@ export default {
         this.gameContainer.appendChild(newCell);
       }
 
+      // calculates the score
       this.scoreHard = this.cellVal.reduce((a, b) => a + b, 0);
       if (this.bestScore < this.scoreEasy && this.level == "easy") {
         this.bestScore = this.scoreEasy;
@@ -224,14 +235,45 @@ export default {
       }
     },
 
+    /** Helper method
+     *  generates a new cell
+     */
     cell(cellVal) {
       var cell = document.createElement("SPAN");
       cell.style = `width: ${this.cellSize}%; height: ${this.cellSize}%`;
-      cellVal !== 0 ? ((cell.style.background = `hsl(${(360 / 100) * cellVal}, 100%, 30%)`), (cell.style.color = "white")) : ((cell.style.background = "none"), (cell.style.color = "none"));
+
+      if (cellVal !== 0) {
+        cell.style.background = `hsl(${cellVal * 100}, 100%, 30%)`;
+        cell.style.color = "white";
+
+        switch (this.keyDirection) {
+          case "ArrowUp": {
+            cell.className = "from-down";
+            break;
+          }
+          case "ArrowDown": {
+            cell.className = "from-up";
+            break;
+          }
+          case "ArrowLeft": {
+            cell.className = "from-right";
+            break;
+          }
+          case "ArrowRight": {
+            cell.className = "from-left";
+            break;
+          }
+        }
+      }
+
       cell.innerHTML = cellVal;
       return cell;
     },
 
+    /** Helper method
+     *  generates a new Number for play with and check if the number is not occupied
+     *  if no available numbers, returns false ( trigger for game over )
+     */
     nextNumber() {
       if (this.cellVal.includes(0)) {
         var nextNumber = Math.floor(Math.random() * (this.cellRow * this.cellRow));
@@ -247,3 +289,182 @@ export default {
   }
 };
 </script>
+
+<style>
+/** Animations
+ *  
+ */
+.enter {
+  animation: bounce 0.7s;
+}
+
+@keyframes bounce {
+  0% {
+    transform: scale(0.1);
+    opacity: 0;
+  }
+
+  70% {
+    transform: scale(1.1);
+    opacity: 1;
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+
+.from-left {
+  animation: from-left 0.2s;
+}
+
+@keyframes from-left {
+  0% {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.from-right {
+  animation: from-right 0.2s;
+}
+
+@keyframes from-right {
+  0% {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.from-up {
+  animation: from-up 0.2s;
+}
+
+@keyframes from-up {
+  0% {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.from-down {
+  animation: from-down 0.2s;
+}
+
+@keyframes from-down {
+  0% {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+/** General CSS
+ *  
+ */
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,500,700&subset=cyrillic");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+body {
+  font-family: "Open Sans", Verdana, Geneva, sans-serif;
+  font-size: 0.9em;
+
+  -moz-user-select: none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.icon {
+  font-family: "Material Icons";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.3em;
+  vertical-align: bottom;
+  display: inline-block;
+
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+  -moz-osx-font-smoothing: grayscale;
+  font-feature-settings: "liga";
+}
+
+.flex-center {
+  display: flex;
+  align-items: center;
+}
+
+.space-between {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.bold {
+  font-weight: 700;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  padding: 0;
+  margin: 0;
+}
+
+.button {
+  display: inline-block;
+  background: white;
+  color: dodgerblue;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+  border-bottom: 2px solid lightgray;
+}
+
+.button:hover {
+  box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+}
+
+.button:active {
+  box-shadow: inset 0 1px 0px 1px rgba(0, 0, 0, 0.15), inset 0 1px 7px 4px rgba(0, 0, 0, 0.15);
+}
+
+.blue-container {
+  border: 1px solid dodgerblue;
+  border-radius: 5px;
+  text-align: center;
+}
+
+#game span {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: inset 0 0 0px 7px lightgray;
+  color: gray;
+  width: 25%;
+  height: 25%;
+  font-size: 2em;
+  font-weight: bold;
+}
+</style>
